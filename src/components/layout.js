cad6795dfa2657c521d6eb8c5ba181bbc57dbd17
Bernhard Gschwantner <bernhard@unserwein.at>
2018-07-16 19:15:04 +0200

    added global styles (fonts)

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import { injectGlobal } from 'react-emotion';
 
 // import Header from './header';
 import './layout.css';
+
+injectGlobal`
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300|Playfair+Display:700');
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Playfair Display', serif;
+  font-weight: 700;
+}
+`;
 
 const Layout = ({ children, data }) => (
   <StaticQuery
