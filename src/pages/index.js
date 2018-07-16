3cdbd2396ea32b8847b23b28e67839db812372b8
Bernhard Gschwantner <bernhard@unserwein.at>
2018-07-16 09:50:19 +0200

    added test image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Layout from '../components/layout'
+import Img from 'gatsby-image';
 
-const IndexPage = () => (
+import Layout from '../components/layout';
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Das Bad</h1>
+    <p>Verfliest mit Solnhofener Marmor, Fußbodenheizung inklusive.</p>
+    <Img title="Bad" alt="Bad" fluid={data.file.childImageSharp.fluid} />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query ImageQuery {
+    file(name: { eq: "bad-n" }) {
+      childImageSharp {
+        fluid(maxWidth: 1280) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
