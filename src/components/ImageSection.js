ca23bb759b7abaaf85a09df8f05302cea2436284
Bernhard Gschwantner <bernhard@unserwein.at>
2018-07-16 19:15:23 +0200

    styled the ImageSection (flex layout)

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import FluidImage from '../components/FluidImage';
+import styled from 'react-emotion';
+
+const Article = styled('article')`
+  display: flex;
+  align-items: center;
+`;
+
+const Main = styled('main')`
+  flex: 0 38.1966%;
+  padding: 16px;
+`;
+
+const Aside = styled('aside')`
+  flex: 1;
+  width: 100%;
+
+  .gatsby-image-wrapper div:first-child {
+    padding-bottom: 100vh !important;
+  }
+`;
 
 const ImageSection = ({ file, title, alt, children }) => (
-  <article>
-    <main>{children}</main>
-    <aside>
+  <Article>
+    <Main>{children}</Main>
+    <Aside>
       <FluidImage title={title} alt={alt} file={file} />
-    </aside>
-  </article>
+    </Aside>
+  </Article>
 );
 
 export default ImageSection;
