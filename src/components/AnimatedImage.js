3d2250ca57a0992ea1deef617595e5e69462616d
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-03 00:06:10 +0200

    added a subtle scaling transition to the Image

--- src/components/AnimatedImage.js
+++ src/components/AnimatedImage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import posed from 'react-pose';
 import FluidImage from './FluidImage';
 
-const transition = { x: { ease: 'easeOut' } };
-const transparent = { opacity: 0, transition };
-const opaque = { opacity: 1, transition };
-const right = { x: '100%', transition };
-const center = { x: '0%', transition };
-const left = { x: '-100%', transition };
+const transition = {
+  x: { ease: 'easeOut' },
+  scale: { delay: 250, duration: 700 },
+};
+const transparent = { opacity: 0, scale: 1.025, transition };
+const opaque = { opacity: 1, scale: 1, transition };
+const right = { x: '100%', scale: 1.025, transition };
+const center = { x: '0%', scale: 1, transition };
+const left = { x: '-100%', scale: 1.025, transition };
 
 const AnimatedImageWrapper = posed.div({
   entering: transparent,
@@ -18,6 +21,10 @@ const AnimatedImageWrapper = posed.div({
   enteringPrevious: left,
   enterPrevious: center,
   exitPrevious: right,
+  enteringLeft: transparent,
+  enterLeft: opaque,
+  enteringRight: transparent,
+  enterRight: opaque,
 });
 
 const AnimatedImageSection = props => (
