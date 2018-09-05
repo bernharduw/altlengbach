ae0f6cb6b99639b14d11162469cde687a9f8c4a3
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-05 10:57:56 +0200

    moved the image to the top in portrait view

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,11 @@ import styled from 'react-emotion';
 const Article = styled('article')`
   flex: 1;
   overflow: scroll; // To enable z-index for the plans.
+  display: flex;
+  flex-direction: column-reverse;
 
   @media (orientation: landscape) {
-    display: flex;
+    flex-direction: row;
     align-items: stretch;
   }
 `;
