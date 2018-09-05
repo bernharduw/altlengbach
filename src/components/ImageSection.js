d1379024bd3c418d586538741c00415a0a48bd24
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-05 10:15:19 +0200

    changed the image height for portrait

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -3,21 +3,21 @@ import styled from 'react-emotion';
 const ImageSection = styled('aside')`
   flex: 1;
   width: 100%;
-  height: 100%;
 
   @media (orientation: landscape) {
     overflow: hidden;
-  }
-
-  // Fix the gatsby image.
-  .gatsby-image-wrapper,
-  .gatsby-image-outer-wrapper {
     height: 100%;
-  }
 
-  .gatsby-image-wrapper div:first-child {
-    height: 100%;
-    padding-bottom: 0 !important;
+    // Fix the gatsby image.
+    .gatsby-image-wrapper,
+    .gatsby-image-outer-wrapper {
+      height: 100%;
+    }
+
+    .gatsby-image-wrapper div:first-child {
+      height: 100%;
+      padding-bottom: 0 !important;
+    }
   }
 
   // Fix the SVG background.
